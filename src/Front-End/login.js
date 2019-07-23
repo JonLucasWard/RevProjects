5b466662566b11e02f2945db0b4a2e4877951c07
JonLucasWard <jonlucasward@gmail.com>
2019-07-23 14:15:22 -0400

    Attempt to Communicate between Page and Server

---
+++ src/Front-End/login.js
@@ -0,0 +1,507 @@
+main = document.getElementById("main");
+navBar = document.getElementById("NavBar");
+display = document.getElementById("display");
+logout = document.getElementById("logout");
+title = document.getElementById("title");
+login = document.getElementById("Login");
+
+// make a function that makes the following addEventListener, call it on need to remake
+async function loginMaker() {
+  login.addEventListener("click", function () {
+    var usrnam = document.getElementById("text-box")["value"];
+    var passy = document.getElementById("passy")["value"]; // required me to not have password field
+    const rawResponse = fetch("http://localhost:3000/login", {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'UserName': usrnam,
+        'Password': passy
+      })
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+
+    /*.then(function (data) {
+        while (display.firstChild) {
+          display.removeChild(display.firstChild);
+        }
+        console.log(body);
+        userViewMaker();
+        console.log("Request success: ", data);
+      })
+      .catch(function (error) {
+        console.log("Request failure: ", error);
+        console.log("Bad Input");
+        while (display.firstChild) {
+          display.removeChild(display.firstChild);
+        }
+        var reject = document.createElement("p");
+        reject.innerText = "Bad Input.";
+        display.appendChild(reject);
+      });*/
+  });
+  /*switch (usrnam) {
+    case "FM":
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      FMViewMaker();
+      break;
+    case "Admin":
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      adminViewMaker();
+      break;
+    default:
+      console.log("Bad Input");
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      var reject = document.createElement("p");
+      reject.innerText = "Bad Input.";
+      display.appendChild(reject);
+      break;*/
+}
+
+loginMaker();
+
+logout.addEventListener("click", function () {
+  // clear body
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+  while (navBar.firstChild) {
+    navBar.removeChild(navBar.firstChild);
+  }
+  // remake login
+  var introPara = document.createElement("p");
+  introPara.innerText = "Please login.";
+  main.appendChild(introPara);
+  var username = document.createElement("input");
+  username.setAttribute("id", "text-box");
+  username.setAttribute("type", "text");
+  username.setAttribute("placeholder", "Username");
+  main.appendChild(username);
+  var password = document.createElement("input");
+  password.setAttribute("id", "passy");
+  password.setAttribute("type", "password");
+  password.setAttribute("placeholder", "Password");
+  main.appendChild(password);
+  var newlog = document.createElement("input");
+  newlog.setAttribute("type", "submit");
+  newlog.setAttribute("id", "Login");
+  main.appendChild(newlog);
+  login = document.getElementById("Login");
+  loginMaker();
+  console.log("I finished logging out");
+  // re-hide logout
+});
+
+function userViewMaker() {
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+  while (navBar.firstChild) {
+    navBar.removeChild(navBar.firstChild);
+  }
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  var user = document.createElement("button");
+  user.setAttribute("id", "UserInfo");
+  user.innerText = "Your Information";
+  navBar.appendChild(user);
+  var submitReim = document.createElement("button");
+  submitReim.setAttribute("id", "submitReim");
+  submitReim.innerText = "Submit Reimbursement";
+  navBar.appendChild(submitReim);
+  main.innerText = "Welcome User, please select an option from the nav bar";
+
+  user.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+    main.innerText =
+      "In the display port is your information at this company, User.";
+    display.innerText = "I should have some information, or something.";
+  });
+
+  submitReim.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+    /* var postReim = document.createElement('form');
+        main.appendChild(postReim);
+        form forces page restart, not ideal for a one page app*/
+
+    var amount = document.createElement("input");
+    amount.setAttribute("id", "amount");
+    amount.setAttribute("type", "number");
+    amount.setAttribute("placeholder", "123.45");
+    main.appendChild(amount);
+    var description = document.createElement("input");
+    description.setAttribute("id", "description");
+    description.setAttribute("type", "text");
+    description.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(description);
+
+    // rType should be a drop-menu list selector
+    var rType = document.createElement("input");
+    rType.setAttribute("id", "rType");
+    rType.setAttribute("type", "text");
+    rType.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(rType);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      display.innerText = "I display the resulting reimbursement here!";
+    });
+  });
+}
+
+function FMViewMaker() {
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+  while (navBar.firstChild) {
+    navBar.removeChild(navBar.firstChild);
+  }
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  var user = document.createElement("button");
+  user.setAttribute("id", "UserInfo");
+  user.innerText = "Search By UserID";
+  navBar.appendChild(user);
+
+  var submitReim = document.createElement("button");
+  submitReim.setAttribute("id", "submitReim");
+  submitReim.innerText = "Submit Reimbursement";
+  navBar.appendChild(submitReim);
+
+  var upd8Reim = document.createElement("button");
+  upd8Reim.setAttribute("id", "upd8Reim");
+  upd8Reim.innerText = "Update Reimbursement";
+  navBar.appendChild(upd8Reim);
+
+  var searchAllReimS = document.createElement("button");
+  searchAllReimS.setAttribute("id", "searchAllReim");
+  searchAllReimS.innerText = "Search By Status";
+  navBar.appendChild(searchAllReimS);
+
+  main.innerText = "Welcome FM, please select an option from the nav bar";
+
+  user.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+    var userId = document.createElement("input");
+    userId.setAttribute("id", "userId");
+    userId.setAttribute("type", "number");
+    userId.setAttribute("placeholder", "1");
+    main.appendChild(userId);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      display.innerText = "I display the resulting user here!";
+    });
+  });
+
+  submitReim.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+    /* var postReim = document.createElement('form');
+        main.appendChild(postReim);
+        form forces page restart, not ideal for a one page app*/
+
+    var amount = document.createElement("input");
+    amount.setAttribute("id", "amount");
+    amount.setAttribute("type", "number");
+    amount.setAttribute("placeholder", "123.45");
+    main.appendChild(amount);
+    var description = document.createElement("input");
+    description.setAttribute("id", "description");
+    description.setAttribute("type", "text");
+    description.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(description);
+
+    // rType should be a drop-menu list selector
+    var rType = document.createElement("input");
+    rType.setAttribute("id", "rType");
+    rType.setAttribute("type", "text");
+    rType.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(rType);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      display.innerText = "I display the resulting reimbursement here!";
+    });
+  });
+
+  upd8Reim.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+
+    var amount = document.createElement("input");
+    amount.setAttribute("id", "amount");
+    amount.setAttribute("type", "number");
+    amount.setAttribute("placeholder", "123.45");
+    main.appendChild(amount);
+    var description = document.createElement("input");
+    description.setAttribute("id", "description");
+    description.setAttribute("type", "text");
+    description.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(description);
+
+    // rType and rStatus should be a drop-menu list selector
+    var rType = document.createElement("input");
+    rType.setAttribute("id", "rType");
+    rType.setAttribute("type", "text");
+    rType.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(rType);
+    var rStatus = document.createElement("input");
+    rStatus.setAttribute("id", "rStatus");
+    rStatus.setAttribute("type", "text");
+    rStatus.setAttribute("placeholder", "Reimbursement Current Status.");
+    main.appendChild(rStatus);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      console.log("The submission ran!");
+      display.innerText = "I display the resulting reimbursement here!";
+    });
+  });
+
+  searchAllReimS.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+
+    var input = document.createElement("input");
+    input.setAttribute("id", "input");
+    // drop down menu for this one
+    input.setAttribute("type", "number");
+    main.appendChild(input);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      display.innerText = "I display the resulting list here!";
+    });
+  });
+}
+
+function adminViewMaker() {
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+  while (navBar.firstChild) {
+    navBar.removeChild(navBar.firstChild);
+  }
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  var user = document.createElement("button");
+  user.setAttribute("id", "UserInfo");
+  user.innerText = "Your Information";
+  navBar.appendChild(user);
+
+  var submitReim = document.createElement("button");
+  submitReim.setAttribute("id", "submitReim");
+  submitReim.innerText = "Submit Reimbursement";
+  navBar.appendChild(submitReim);
+
+  var userUpd8 = document.createElement("button");
+  userUpd8.setAttribute("id", "userUpd8");
+  userUpd8.innerText = "Update User";
+  navBar.appendChild(userUpd8);
+
+  main.innerText = "Welcome Admin, please select an option from the nav bar";
+
+  user.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+    main.innerText =
+      "In the display port is your information at this company, User.";
+    display.innerText = "I should have some information, or something.";
+  });
+
+  submitReim.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+    /* var postReim = document.createElement('form');
+        main.appendChild(postReim);
+        form forces page restart, not ideal for a one page app*/
+
+    var amount = document.createElement("input");
+    amount.setAttribute("id", "amount");
+    amount.setAttribute("type", "number");
+    amount.setAttribute("placeholder", "123.45");
+    main.appendChild(amount);
+    var description = document.createElement("input");
+    description.setAttribute("id", "description");
+    description.setAttribute("type", "text");
+    description.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(description);
+
+    // rType should be a drop-menu list selector
+    var rType = document.createElement("input");
+    rType.setAttribute("id", "rType");
+    rType.setAttribute("type", "text");
+    rType.setAttribute(
+      "placeholder",
+      "Describe the purpose of your reimbursement."
+    );
+    main.appendChild(rType);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      display.innerText = "I display the resulting reimbursement here!";
+    });
+  });
+
+  userUpd8.addEventListener("click", function () {
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+
+    var userName = document.createElement("input");
+    userName.setAttribute("id", "text");
+    userName.setAttribute("type", "username");
+    userName.setAttribute("placeholder", "Username");
+    main.appendChild(userName);
+    var password = document.createElement("input");
+    password.setAttribute("id", "password");
+    password.setAttribute("type", "password");
+    password.setAttribute("placeholder", "Password");
+    main.appendChild(password);
+    var firstName = document.createElement("input");
+    firstName.setAttribute("id", "firstName");
+    firstName.setAttribute("type", "text");
+    firstName.setAttribute("placeholder", "First Name");
+    main.appendChild(firstName);
+    var lastName = document.createElement("input");
+    lastName.setAttribute("id", "lastName");
+    lastName.setAttribute("type", "text");
+    lastName.setAttribute("placeholder", "Last Name");
+    main.appendChild(lastName);
+    var emailAdd = document.createElement("input");
+    emailAdd.setAttribute("id", "emailAdd");
+    emailAdd.setAttribute("type", "text");
+    emailAdd.setAttribute("placeholder", "Abc123@example.com");
+    main.appendChild(emailAdd);
+
+    var submitter = document.createElement("button");
+    submitter.setAttribute("id", "submitter");
+    submitter.innerText = "Submit";
+    main.appendChild(submitter);
+
+    submitter.addEventListener("click", function () {
+      while (display.firstChild) {
+        display.removeChild(display.firstChild);
+      }
+      display.innerText = "I display the resulting update here!";
+    });
+  });
+}
--- src/app.ts
+++ src/app.ts
@@ -7,34 +7,35 @@
 // need express for file dependencies
 // brackets are to get types
 /*{Request, Response} from <- use if something goes wrong*/
-import bodyParser from 'body-parser'; // sees if content of request is json, then turn it into object for JS
-import express from 'express'; // allows for communication with HTTP requests
-import loginRouter from './routers/loginRouter'; // series of functions for the login URL
-import reimRouter from './routers/reimRouter'; // functions for the reimbursement URL
-import userRouter from './routers/userRouter'; // functions for the user URL
-import db from './util/pg-connector'; // connector to the database
+import bodyParser from "body-parser"; // sees if content of request is json, then turn it into object for JS
+import express from "express"; // allows for communication with HTTP requests
+import loginRouter from "./routers/loginRouter"; // series of functions for the login URL
+import reimRouter from "./routers/reimRouter"; // functions for the reimbursement URL
+import userRouter from "./routers/userRouter"; // functions for the user URL
+import db from "./util/pg-connector"; // connector to the database
 
 const app = express(); // opens an instance of express to communicate with HTTP, this is the "app" in total
 const port = 3000; // the local port we are communicating with to do this
 const port_handler = app.listen(port, () =>
-    console.log(`ERS app listening on port ${port}!`),
+  console.log(`ERS app listening on port ${port}!`)
 ); // port_handler is what "listens" for actions done on the port, the console log is just to know that it's on
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // turning on bodyParser? Could be a useless call.
 app.use(express.json()); // turning on express? Could be a useless call.
 
 app.use((req, res, next) => {
-    console.log('Request received for ' + req.url);
-    next();
+  console.log("Request received for " + req.url);
+  next();
 }); // Every time a request is made through this application, this function will tell the developer
 // What URL is being requested by the user, so at least you KNOW where something breaks, kinda'.
 
-app.use('/login', loginRouter); // If the user wants to use the /login path, it sends them there
-app.use('/reimbursements', reimRouter); // As above, but for reimbursements
-app.use('/users', userRouter); // As above, note that first you indicate the path and then the "package" of the path
+app.use("/login", loginRouter); // If the user wants to use the /login path, it sends them there
+app.use("/reimbursements", reimRouter); // As above, but for reimbursements
+app.use("/users", userRouter); // As above, note that first you indicate the path and then the "package" of the path
 
-process.on('SIGINT', () => {
-    db.end().then(() => console.log('pool has ended'));
-    port_handler.close();
+process.on("SIGINT", () => {
+  db.end().then(() => console.log("pool has ended"));
+  port_handler.close();
 }); // When a user INTerrupts the SIGnal going to the app, turn off the database,
 // close the database connection and tell us.
